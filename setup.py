88f66e20dd45383e5e353fb1796b0eb774ef3e6b
Aravind Kumaraguru <5566152+akumaraguru@users.noreply.github.com>
2019-09-04 14:13:57 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# Required dependencies
+required = [
+    'joblib',
+    'numpy',
+    'tqdm',
+]
+
+setup(
+    name = "smallab",
+    version = "0.0.0",
+    url='https://github.com/octopuscabbage/smallab',
+    packages=find_packages(),
+    install_requires=required,
+    license = "BSD 2-Clause License",
+    description = "smallab (Small Lab) is an experiment framework designed " + \
+        "to be easy to use with your experiment",
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+)
