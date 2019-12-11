9e44bccdfcb8cc52c77f806bd2513c84c69c6b37
Chris Denniston <octopuscabbage@gmail.com>
2019-12-11 14:11:42 -0800

    callback-enfeebler divergence

--- smallab/utilities/logger_callbacks.py
+++ smallab/utilities/logger_callbacks.py
@@ -6,31 +6,40 @@ import traceback
 import typing
 import pprint
 
-def configure_logging_default(experiment_name):
-    folder_loc = os.path.join("experiment_runs",experiment_name,"logs")
-    file_loc = os.path.join(folder_loc, str(datetime.datetime.now()) + ".log")
-    if not os.path.exists(folder_loc):
-        os.makedirs(folder_loc)
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
-        handlers=[
-            logging.FileHandler(file_loc),
-            logging.StreamHandler()
-        ])
+from smallab.callbacks import CallbackManager
 
-def pretty_printer():
-    return pprint.PrettyPrinter()
 
-def logging_on_specification_complete_callback(specification:typing.Dict,result:typing.Dict) -> typing.NoReturn:
-    pp = pretty_printer()
-    specification_string = pp.pformat(specification)
-    result_string = pp.pformat(result)
-    logging.info("\nSpecification Complete\nSpecification:\n{}\nResult:\n{}".format(specification_string,result_string))
-def logging_on_specification_failure_callback(exception:Exception,specification:typing.Dict) -> typing.NoReturn:
-    pp = pretty_printer()
-    traceback_string = traceback.format_exc()
-    specification_string = pp.pformat(specification)
-    logging.error("\nSpecification Failure\nSpecification:\n{}\nException:\n{}".format(specification_string,traceback_string))
+class LoggingCallback(CallbackManager):
+    @staticmethod
+    def pretty_printer():
+        return pprint.PrettyPrinter()
 
+    def set_experiment_name(self,name):
+        super().set_experiment_name(name)
+        folder_loc = os.path.join("experiment_runs", name, "logs")
+        file_loc = os.path.join(folder_loc, str(datetime.datetime.now()) + ".log")
+        if not os.path.exists(folder_loc):
+            os.makedirs(folder_loc)
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+            handlers=[
+                logging.FileHandler(file_loc),
+                logging.StreamHandler()
+            ])
+
+
+    def on_specification_complete(self, specification: typing.Dict, result: typing.Dict) -> typing.NoReturn:
+        pp = LoggingCallback.pretty_printer()
+        specification_string = pp.pformat(specification)
+        result_string = pp.pformat(result)
+        logging.info(
+            "\nSpecification Complete\nSpecification:\n{}\nResult:\n{}".format(specification_string, result_string))
+
+    def on_specification_failure(self, exception: Exception, specification: typing.Dict) -> typing.NoReturn:
+        pp = LoggingCallback.pretty_printer()
+        traceback_string = traceback.format_exc()
+        specification_string = pp.pformat(specification)
+        logging.error("\nSpecification Failure\nSpecification:\n{}\nException:\n{}".format(specification_string,
+                                                                                           traceback_string))
 
