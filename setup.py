61046ed6b41cedaa63d923bc874ed1f69f825509
Chris Denniston <cdennist@taochen-S5520SC>
2020-06-16 19:19:22 -0700

    undo bad releas

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ required = [
 
 setup(
     name="smallab",
-    version="1.5.3",
+    version="1.6.1",
     url='https://github.com/octopuscabbage/smallab',
     packages=find_packages(),
     install_requires=required,
