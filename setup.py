101e37b23e58fac4da0f2356719a0145b759de4a
Chris Denniston <cdennist@usc.edu>
2020-10-08 14:03:17 -0700

    experimental diff naming

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ required = [
 
 setup(
     name="smallab",
-    version="1.8.2",
+    version="1.9.0",
     url='https://github.com/octopuscabbage/smallab',
     packages=find_packages(),
     install_requires=required,
